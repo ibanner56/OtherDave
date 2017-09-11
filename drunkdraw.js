59e60207614b28b781213f002f70a03b982a7ef8
Isaac Banner <ibanner56@gmail.com>
2017-09-10 21:31:26 -0400

    Set up base for drunkdraw params to be processed by Dave.

--- drunkdraw.js
+++ drunkdraw.js
@@ -1,46 +1,62 @@
-'use strict';
+"use strict";
 
-var moment = require('moment');
+var moment = require("moment");
 
-function drunkdraw() {
-    var draw = require('./drunkdraw.json');
-    var base = 'The next drunkdraw is ';
+function drunkdraw(logger, user, message) {
+    logger.info("DrunkDraw - user: " + user + ", message: " + message);
 
-    if(draw.date) {
-	base += draw.date;
-    } else {
-        var today = moment();
-	var drawdate = moment({year: today.year(), month: today.month()});
-    	drawdate.date(drawdate.date() + 14 - drawdate.day() % 7);
+    var args = message.split(" ").slice(1);
+    if(args.length > 0) {
+    	logger.debug(args);
+		if(user != "MercWorks"){
+			return "Mama Mia! Only Dave can do that!";
+		}
+    }
+    else {
+    	var draw = require("./drunkdraw.json");
+	  	var base = "The next drunkdraw is ";
 
-	if(today.date() > drawdate.date()) {
-	    drawdate.month(drawdate.month() + 1);
-	    drawdate.date(drawdate.date() + 14 - drawdate.day() % 7);
+    	if (draw.date) {
+            base += draw.date;
+    	} else {
+            var today = moment();
+            var drawdate = moment({year: today.year(), month: today.month()});
+        
+			if(drawdate.date() == 0) {
+	    		drawdate.date(8);
+			} else {
+			    drawdate.date(drawdate.date() + 14 - drawdate.day() % 7);
+			}
+
+        	if (today.date() > drawdate.date()) {
+           	 	drawdate.month(drawdate.month() + 1);
+            	drawdate.date(drawdate.date() + 14 - drawdate.day() % 7);
+        	}
+
+        	base += drawdate.format("dddd[,] MMMM Do ");
+    	}
+
+    	base += "at ";
+
+    	if (draw.time) {
+    	    base += draw.time;
+    	} else {
+    	    base += "4:00 pm PST. ";
+    	}
+
+    	if (draw.theme) {
+       	 	base += "The theme is " + draw.theme + ". ";
+        	if (draw.references) {
+            	base += "\nJust this once, references ARE allowed.";
+        	} else {
+            	base += "\nAs always, references are NOT allowed.";
+        	}
+    	} else {
+        	base += "Theme is still tbd.";
+    	}
+
+    	return base;
 	}
-
-	base += drawdate.format('dddd[,] MMMM Do ');
-    }
-
-    base += 'at ';
-
-    if(draw.time) {
-        base += draw.time;
-    } else {
-        base += '4:00 pm PST. ';
-    }
-
-    if(draw.theme) {
-        base += 'The theme is ' + draw.theme + '. ';
-	if(draw.references) {
-	    base += '\nJust this once, references ARE allowed.';
-	} else {
-	    base += '\nAs always, references are NOT allowed.';
-	}
-    } else {
-        base += 'Theme is still tbd.';
-    }
-
-    return base;
 }
 
 module.exports = drunkdraw;
--- otherdave.js
+++ otherdave.js
@@ -1,46 +1,47 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var moment = require('moment');
+"use strict";
 
-var drunkdraw = require('./drunkdraw.js');
+var Discord = require("discord.io");
+var logger = require("winston");
 
-var auth = require('./auth.json');
+var drunkdraw = require("./drunkdraw.js");
+
+var auth = require("./auth.json");
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
-logger.level = 'debug';
+logger.level = "debug";
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun: true
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on("ready", function (evt) {
+    logger.info("Connected");
+    logger.info("Logged in as: ");
+    logger.info(bot.username + " - (" + bot.id + ")");
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on("message", function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.substring(0, 1) == "!") {
+        var args = message.substring(1).split(" ");
         var cmd = args[0];
-       
+
         args = args.splice(1);
-        switch(cmd) {
-            case 'dave':
+        switch (cmd) {
+            case "dave":
                 bot.sendMessage({
                     to: channelID,
-                    message: 'OtherDave is not David'
+                    message: "OtherDave is not David"
                 });
-		break;
-	    case 'drunkdraw':
-	        bot.sendMessage({
-		    to: channelID,
-		    message: drunkdraw()
-		});
+                break;
+            case "drunkdraw":
+                bot.sendMessage({
+                    to: channelID,
+                    message: drunkdraw(logger, user, message)
+                });
                 break;
             // Just add any case commands if you want to..
          }
