dbc2d3683cbdc3aa47bda88ab4ecc8c0022fd9aa
Isaac Banner <ibanner56@gmail.com>
2017-09-19 19:43:12 -0400

    Switch to sending the response from the function, rather than generating a string.

--- otherdave.js
+++ otherdave.js
@@ -5,9 +5,15 @@ var logger = require("winston");
 
 var lib = {};
 
-lib.dave = function() { return "OtherDave is not David."; };
+lib.dave = function(logger, client, user, userID, channelID, args) { 
+				client.sendMessage({
+					to: channelID,
+					message: "OtherDave is not David."
+				}); 
+		   };
 lib.drunkdraw = require("./lib/drunkdraw.js");
 lib.respect = require("./lib/respect.js");
+lib.comic = require("./lib/comic.js");
 
 var auth = require("./data/auth.json");
 // Configure logger settings
@@ -17,31 +23,28 @@ logger.add(logger.transports.Console, {
 });
 logger.level = "debug";
 // Initialize Discord Bot
-var bot = new Discord.Client({
+var client = new Discord.Client({
     token: auth.token,
     autorun: true
 });
-bot.on("ready", function (evt) {
+client.on("ready", function (evt) {
     logger.info("Connected");
     logger.info("Logged in as: ");
-    logger.info(bot.username + " - (" + bot.id + ")");
+    logger.info(client.username + " - (" + client.id + ")");
 });
-bot.on("message", function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == "!") {
-        var args = message.trim().substring(1).split(" ");
+client.on("message", function (user, userID, channelID, message, evt) {
+	if (message.substring(0, 1) == "!") {
+		client.simulateTyping(channelID);
+
+		var args = message.trim().substring(1).split(" ");
         var cmd = args[0];
 
         args = args.splice(1);
 
 		if(cmd in lib) {
-			bot.sendMessage({
-				to: channelID,
-				message: lib[cmd](logger, user, args)
-			});
+			lib[cmd](logger, client, user, userID, channelID, args);
 		} else {
-			bot.sendMessage({
+			client.sendMessage({
 				to: channelID,
 				message: "I'm sorry Dave, I'm afraid I can't do that."
 			});
