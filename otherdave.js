841ee36b25b1176d341c70e6077d3b3ad5f6f5a4
Isaac Banner <isaacba@microsoft.com>
2020-04-09 09:09:09 -0700

    Add boot dlog

--- otherdave.js
+++ otherdave.js
@@ -37,7 +37,8 @@ var client = new Discord.Client({
 client.on("ready", function (evt) {
     logger.info("Connected");
     logger.info("Logged in as: ");
-    logger.info(client.username + " - (" + client.id + ")");
+	logger.info(client.username + " - (" + client.id + ")");
+	dlog(client, "Hi, I'm OtherDave and I'm BACK FOR BUSINESS.");
 });
 client.on("message", function (user, userID, channelID, message, evt) {
 	// OtherDave shouldn't talk to himself.
