3752b034a37b5c596e424d978b0960c887b191fc
ibanner56 <ibanner56@gmail.com>
2020-06-02 14:09:20 -0700

    Check result

--- test/commands/test_haiku.py
+++ test/commands/test_haiku.py
@@ -5,7 +5,8 @@ class HaikuTestCase(unittest.TestCase):
     
     def test_ishaiku(self):
         text = "This one is a test. This poem is a haiku. Refrigerator."
-        self.assertIsNotNone(haiku.parseHaiku(text, False))
+        expected = "*This one is a test.*\n*This poem is a haiku.*\n*Refrigerator.*"
+        self.assertEqual(haiku.parseHaiku(text, False), expected)
 
     def test_isnothaiku(self):
         text = "This one isn't a haiku, stupid."
