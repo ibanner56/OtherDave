a0e99f6dcac969c944406d916d14946050dbcb18
Isaac Banner <isaacba@microsoft.com>
2020-06-04 23:39:14 -0700

    Fix reactions

--- otherdave/util/triggers.py
+++ otherdave/util/triggers.py
@@ -7,18 +7,15 @@ with open("./conf.yaml") as conf:
 async def react(message):
     for emotion in config["emotions"]:
         if(emotion in message.content):
-            emoji = discord.utils.get(message.guild.emojis, name=emotion)
+            emoji = discord.utils.get(message.guild.emojis, name=config["emotions"][emotion])
             if emoji:
-                message.add_reaction(emoji)
+                await message.add_reaction(emoji)
 
 async def pedant(message):
     content = message.content.lower()
 
     if("-ass" in content):
         await message.channel.send(content.replace("-ass ", " ass-"))
-
-    if("mario" in content):
-        await message.channel.send("Mario? Is that like Hop Dude?\n*cough cough*")
 
     if("sonic" in content):
         await message.channel.send("Gotta love that Fast Guy.")
