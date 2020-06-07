4e441000ae7ea4d8b7c9e4024b6dd1150ce52e63
ibanner56 <ibanner56@gmail.com>
2020-06-06 20:18:19 -0700

    Update setup py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 requirements = [
-    "python-dateutil>=2.8.1",
     "discord.py>=1.3.3",
     "inflect>=4.1",
+    "markovify>=0.8.0",
     "pickleDB>=0.9.2",
+    "python-dateutil>=2.8.1",
     "PyYAML>=5.3.1",
     "textstat>=0.6.2"
 ]
@@ -14,6 +15,7 @@ setup(
     url="https://github.com/ibanner56/OtherDave",
     version="2.0.0",
     packages=["otherdave", "otherdave.commands", "otherdave.util"],
+    test_suite="test",
     license="MIT",
     description="OtherDave is not David.",
     include_package_data=True,
