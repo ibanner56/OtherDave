3b9f3ae41a923566449ec958b375c7cf93eb946f
ibanner56 <ibanner56@gmail.com>
2020-06-25 11:27:30 -0700

    Fix issues with mimic for people who don't use punctuation.

--- otherdave/commands/mimic.py
+++ otherdave/commands/mimic.py
@@ -2,6 +2,7 @@ import markovify
 import random
 import re
 
+_makeFailed = "hahaha Oof owie Heck, guess I don't know you well enough to do that."
 loads = {}
 
 def listen(message):
@@ -9,29 +10,42 @@ def listen(message):
         return
 
     user = message.author.id
-    if(user in loads):
-        loads[user] += "\n" + message.content
+    content = message.content
+    if(not content.endswith(".")):
+        # Markovify doesn't treat \n as punctuation...
+        content = content + "."
 
     filename = "./data/markov/" + str(message.author.id) + ".txt"
     with open(filename, "a", encoding="utf-8") as mfile:
-            mfile.write(message.content + "\n")
+            mfile.write("\n" + content)
+    if(user in loads):
+        loads[user] += "\n" + content
 
 async def mimic(client, message, args):
     async with message.channel.typing():
         if(args):
-            user = re.sub("<@!*|>", "", args[0])
+            user = re.sub("<@!*|>", "", args[0]).lower()
         else:
             user = message.author.id
 
         if(not user in loads):
             filename = "./data/markov/" + str(user) + ".txt"
-            with open(filename, "r", encoding="utf-8") as mfile:
-                loads[user] = mfile.read()
+            try:
+                with open(filename, "r", encoding="utf-8") as mfile:
+                    loads[user] = mfile.read()
+            except OSError:
+                return await message.channel.send(_makeFailed) 
 
         model = markovify.Text(loads[user], well_formed=True)
 
-        for i in range(3):
-            sentence = model.make_sentence(tries=1000000, max_words=random.randint(4, 40))
-            await message.channel.send(sentence)
-    
+        result = ""
+        for _ in range(3):
+            sentence = model.make_sentence(tries=1000000, max_words=random.randint(8, 40))
+            if(sentence):
+                result += sentence + "\n"
+
+        if(result):
+            return await message.channel.send(sentence)
+        else:
+            return await message.channel.send(_makeFailed)    
         
