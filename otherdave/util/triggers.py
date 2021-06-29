a6a23156f3a489efa19c4645e91c1192bcc7c0a3
Isaac Banner <ibanner56@gmail.com>
2021-06-29 07:42:11 -0700

    Allow users to enable or disable auto-responses (#101)
    * Allow users to enable or disable auto-responses
    
    * whitespace

--- otherdave/util/triggers.py
+++ otherdave/util/triggers.py
@@ -1,9 +1,14 @@
 import discord
+import pickledb
 import random
 import yaml
 
+_pedantUsage = "*Actually*, the correct usage is '!pedant -me | -stop'."
+
 with open("./conf.yaml") as conf:
     config = yaml.load(conf, Loader=yaml.BaseLoader)
+
+grumps = pickledb.load("./data/grumps.db", True)
 
 async def react(message):
     for emotion in config["emotions"]:
@@ -15,9 +20,26 @@ async def react(message):
                 await message.add_reaction(emoji)
             elif(random.randrange(100) > 99):
                 # Deploy random banjo
-                await message.add_reaction(config["banjo"])
+                await message.add_reaction(config["emotions"]["_banjo"])
+
+async def grump(client, message, args):
+    if (len(args) != 1):
+        await message.channel.send(_pedantUsage)
+    
+    if (args[0] == "-me"):
+        grumps.set(str(message.author.id), False)
+        await message.add_reaction(config["emotions"]["_teacher"])
+    elif (args[0] == "-stop"):
+        grumps.set(str(message.author.id), True)
+        await message.add_reaction(config["emotions"]["_zipit"])
+    else:
+        await message.channel.send(_pedantUsage)
+    
 
 async def pedant(message):
+    if (grumps.get(str(message.author.id))):
+        return
+
     content = message.content.lower()
 
     if("-ass" in content):
