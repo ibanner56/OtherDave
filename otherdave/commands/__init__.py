fd6a09f52e8bb6c89cd2a9d0e2be23c718bb9de5
ibanner56 <ibanner56@gmail.com>
2021-10-06 12:27:20 -0700

    Update commands init

--- otherdave/commands/__init__.py
+++ otherdave/commands/__init__.py
@@ -1,7 +1,9 @@
 # OtherDave/otherdave/commands/__init__.py
 
 from .drunkdraw import *
+from .ignore import *
 from .haiku import *
+from .haunt import *
 from .jabber import *
 from .memory import *
 from .mimic import *
