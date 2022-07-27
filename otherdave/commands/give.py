987fc8683782af5ee804e39eca4890ac1ac0774f
Isaac Banner <ibanner56@gmail.com>
2022-07-26 17:06:35 -0700

    Add users personal inventories

--- otherdave/commands/give.py
+++ otherdave/commands/give.py
@@ -9,17 +9,19 @@ with open("./conf.yaml") as conf:
 
 inventoryKey = "inventory"
 bagsize = int(config["bag_size"])
+userbagsize = int(config["user_bag_size"])
 selftag = "<@" + config["self_id"] + ">"
 
 # TODO: It would be neat if there could be a few different madlibs for these responses.
-_emptyBag = "Aw heck, I'm all out of stuff."
-_inventoryPreface = "Well heck, I've got a whole bunch of stuff. Right now I'm carrying:"
+_emptyBag = "Aw heck, {whos} all out of stuff."
+_inventoryPreface = "Well heck, {whove} got a whole bunch of stuff. Right now {whos} carrying:"
 _unknownThing = "I don't have {thing}, give them one yourself."
 _giftMessage = "Here, {target}, have {thing}."
 _knownThing = "I've already got {thing}!"
 _takeMessage = "Here, have {thing}."
 _thanksMessage = selftag + " is now carrying {thing}."
 _thanksfulMessage = selftag + " dropped {oldThing} and is now carrying {newThing}."
+_userfulMessage = "..\n\t*...it looks like you've dropped {thing} - I hope it wasn't important.*"
 
 thinger = Thinger()
 infl = inflect.engine()
@@ -28,9 +30,9 @@ bag = pickledb.load("./data/bag.db", True)
 if (not bag.exists(inventoryKey)):
     bag.lcreate(inventoryKey)
 
-def give(target = selftag, thing = "something"):
+def give(author, target = selftag, thing = "something"):
     if (target != selftag):
-        return take(target, thing)
+        return take(author, target, thing)
     
     if (thing == "something"):
         thing = infl.a(thinger.make())
@@ -38,18 +40,20 @@ def give(target = selftag, thing = "something"):
     if (bag.lexists(inventoryKey, thing)):
         return _knownThing.format(thing = thing)
     
+    # Put the new thing in the bag
+    bag.ladd(inventoryKey, thing)
+
+    # Throw an old thing out if we're all full
     if (bag.llen(inventoryKey) >= bagsize):
         oldThing = bag.lpop(inventoryKey, 0)
-        bag.ladd(inventoryKey, thing)
         return _thanksfulMessage.format(oldThing = oldThing, newThing = thing)
+    else:
+        return _thanksMessage.format(thing = thing)
 
-    bag.ladd(inventoryKey, thing)
-    return _thanksMessage.format(thing = thing)
 
-
-def take(target, thing):
+def take(author, target, thing):
     if (bag.llen(inventoryKey) == 0):
-        return _emptyBag
+        return _emptyBag.format(whos = "I'm")
 
     if (thing == "something"):
         bagIndex = random.randint(0, bag.llen(inventoryKey)-1)
@@ -61,17 +65,57 @@ def take(target, thing):
         return _unknownThing.format(thing = thing)
 
     if (target == "me"):
-        return _takeMessage.format(thing = gift)
+        target = author
+
+    if (not bag.exists(target)):
+        bag.lcreate(target)
+
+    # Put the new thing in the bag
+    bag.ladd(target, gift)
+
+    if (target == author):
+        response = _takeMessage.format(thing = gift)
     else:
-        return _giftMessage.format(target = target, thing = gift)
+        response = _giftMessage.format(target = target, thing = gift)
 
-def inventory():
-    if (bag.llen(inventoryKey) == 0):
-        return _emptyBag
+    # Throw an old thing out if we're all full
+    if (bag.llen(target) >= userbagsize):
+        oldThing = bag.lpop(target, 0)
+        response += _userfulMessage.format(thing = oldThing)
 
-    inventoryString = _inventoryPreface
+    return response
+
+def inventory(author, user):
+    if (user is None or user == selftag):
+        whove = "I've"
+        whos = "I'm"
+
+        if (bag.llen(inventoryKey) == 0):
+            return _emptyBag.format(whos = whos)
+
+        backpack = bag.lgetall(inventoryKey)
+
+    else:
+        if (author == user):
+            whove = "you've"
+            whos = "you're"
+        else:
+            whove = "they've"
+            whos = "they're"
+
+        if (bag.exists(user)):
+            if (bag.llen(user) == 0):
+                return _emptyBag.format(whos = whos)
+            
+            backpack = bag.lgetall(user)
+
+        else:
+            return _emptyBag.format(whos = whos)
+
+        
+    inventoryString = _inventoryPreface.format(whove = whove, whos = whos)
     
-    for thing in bag.lgetall(inventoryKey):
+    for thing in backpack:
         inventoryString += "\n\t- " + thing
 
     return inventoryString
