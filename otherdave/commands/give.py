2cd3cc0e3a5f19e0716e3b288118efe67640119b
Isaac Banner <ibanner56@gmail.com>
2022-07-30 12:30:36 -0700

    More fixes whoopee

--- otherdave/commands/give.py
+++ otherdave/commands/give.py
@@ -188,7 +188,7 @@ def use(author, *args):
     if (len_args < 2 and args[0] == "-my"):
         return _useUsage
     
-    args = args[0] + " ".join(args[1:]) if args[0] == "-my" else " ".join(args)
+    args = args[0] + [" ".join(args[1:])] if args[0] == "-my" else [" ".join(args)]
 
     mention = inventoryKey if len_args == 1 else author.mention
     thing = args[0] if len_args == 1 else args[1]
