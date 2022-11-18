f74726fe7807bfc8ccf15de8519c37cc984bdcde
Isaac Banner <ibanner56@gmail.com>
2022-11-17 20:09:18 -0800

    Address byte length and signing bugs

--- otherdave/util/stringmischief.py
+++ otherdave/util/stringmischief.py
@@ -1,7 +1,10 @@
 # Some helpers for stringMischief down below
 def _byteWiseAdd(sthing, ithing):
     sbytes = sthing.encode("utf-8")
-    ibytes = ithing.to_bytes(len(sbytes), "big")
+    ibytes = ithing.to_bytes(max(len(sbytes), (ithing.bit_length() + 7) // 8), "big", signed=True)
+    if (len(sbytes) < len(ibytes)):
+        padbytes = (0).to_bytes((len(ibytes)-len(sbytes)), "big", signed=True)
+        sbytes = padbytes + sbytes
     return bytes(list(map(lambda x, y: min(x + y, 255), sbytes, ibytes)))
 
 def _weirdIntAddAndReByte(sthing, ithing):
@@ -16,10 +19,14 @@ def _dodgeControlBytes(byteVal):
 
 # A few insane ways to add an int to a string
 stringMischief = [
-    lambda sthing, ithing: str(sthing) + str(ithing),                           # (STR) Simple string concat
-    lambda sthing, ithing: str(_byteWiseAdd(sthing, ithing), "utf-8"),          # (STR) Bytewise addition
-    lambda sthing, ithing: int.from_bytes(_byteWiseAdd(sthing, ithing), "big"), # (INT) Bytewise addition
-    lambda sthing, ithing: sum(sthing.encode("utf-8")) + ithing,                # (INT) Sum the string bytes and add the int
-                                                                                # (STR) Uhhhhhh, don't ask me about this next one
+    # (STR) Simple string concat
+    lambda sthing, ithing: str(sthing) + str(ithing),
+    # (INT) Sum the string bytes and add the int
+    lambda sthing, ithing: sum(sthing.encode("utf-8")) + ithing,
+    # (INT) Bytewise addition   
+    lambda sthing, ithing: int.from_bytes(_byteWiseAdd(sthing, ithing), "big", signed=True), 
+    # (STR) Bytewise addition
+    lambda sthing, ithing: "".join(list(map(lambda x: chr(_dodgeControlBytes(x)), _byteWiseAdd(sthing, ithing)))),
+    # (STR) Uhhhhhh, don't ask me about this next one
     lambda sthing, ithing: "".join(list(map(lambda x: chr(_dodgeControlBytes(x)), _weirdIntAddAndReByte(sthing, ithing))))
 ]
