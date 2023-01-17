5a1522658fabb0e9752259e756b68b8c19f78181
ibanner56 <ibanner56@gmail.com>
2023-01-17 15:31:19 -0800

    Add trigger for random unit conversion

--- otherdave/util/triggers.py
+++ otherdave/util/triggers.py
@@ -1,9 +1,26 @@
 import discord
+import json
 import pickledb
 import random
 from otherdave.util import config, constants
 
 grumps = pickledb.load("./data/grumps.db", True)
+
+with open("./data/units.json") as unitsf:
+    units = json.load(unitsf)
+
+commonConversions = [
+    lambda x: x * 100.0, 
+    lambda x: x * (5.0/9.0) + 32,
+    lambda x: x * 2.54,
+    lambda x: x * 36,
+    lambda x: x * 5280,
+    lambda x: x * 6894.76,
+    lambda x: x * 0.453592,
+    lambda x: x * 0.907185,
+    lambda x: x * 28.3495,
+    lambda x: x * 3.78514,
+]
 
 async def react(message):
     for emotion in config.emotions:
@@ -46,3 +63,15 @@ async def pedant(message):
 
     if("classic" in content):
         await message.channel.send("*classic*")
+
+    await converter(message, content)
+
+async def converter(message, content):
+    content = content.split(" ")
+
+    for i in range(0, len(content)):
+        if content[i] in units["unit"] or content[i] in units["units"]:
+            if (i-1 >= 0) and content[i-1].isnumeric():
+                value = random.choice(commonConversions)(float(content[i-1]))
+                unit = random.choice(units["units"])
+                await message.channel.send(f"{content[i-1]} {content[i]} is equivalent to {value} {unit}")
